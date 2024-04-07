30b254935f73f3f30bba5c5f8e88b56498ec96f4
ujjwal dhungel <dhungelujjwal441@gmail.com>
2024-04-07 11:59:52 +0545

    login page tested

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,32 @@
+
+
+/// <reference types = "cypress"/>
+
+describe('Login Test',()=>{
+    beforeEach(()=>{
+
+      cy.visit('/login')
+
+    })
+
+    it('Success test',()=>{
+        cy.get('.login-portal-heading').contains(/customer login/i).should('exist')
+        cy.get('#login').should("be.visible").type('dudhe_bhanubhakta_dhungel')
+        cy.get('#password').should("be.visible").type('123456')
+        cy.get('#submit').should("contain.value","Sign in")
+        cy.get('#submit').click()
+        cy.url().should("include","/dashboard")
+        
+    })
+
+    it('Failed test',()=>{
+        cy.get('.login-portal-heading').contains(/customer login/i)
+       cy.get('#login').should("be.visible").type('ujjwal')  
+        cy.get('#password').should("be.visible").type('123')
+        cy.get('#submit').should("contain.value","Sign in")
+        cy.get('#submit').click()
+        cy.get('#static').contains(/login or password incorrect/i).should("exist")
+        
+        
+    })
+})
